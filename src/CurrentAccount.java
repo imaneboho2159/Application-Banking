21a8cddb744944dc016d433d9d37b92f45af0858
imaneboho2159 <imaboh21@gmail.com>
2024-12-26 17:50:08 +0100

    Add the two methode add and display

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CurrentAccount extends Account {
-    Double bankCharges ;
-    static Scanner scanner = new Scanner(System.in);
 
-    public CurrentAccount(long number, double sold, Client client) {
-        super(number, sold, client);
+public class CurrentAccount extends Account {
+    static Double bankCharges;
+    private Client clients;
+
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<CurrentAccount> currentAccount = new ArrayList<>();
+
+    public CurrentAccount(long accountNumber, double initialBalance, Client clients) {
+        super(accountNumber, initialBalance, clients);
+    }
+
+    public CurrentAccount() {
+        super();
     }
 
     public Double getBankCharges() {
@@ -15,27 +25,70 @@ public class CurrentAccount extends Account {
     public void setBankCharges(Double bankCharges) {
         this.bankCharges = bankCharges;
     }
-    public static void MenuCurrentAccount() {
+
+    public void MenuCurrentAccount() {
         int choice = 1;
         while (choice != 0) {
-        System.out.println("1- Add Current Account\n"+ "2 -Display Current Account\n" + "3- Update Current Account\n" + "4-Delete Current Account\n" +" 0- Get out \n"+"  Enter your choice");
-        choice = scanner.nextInt();
-        scanner.nextLine();
-        switch(choice) {
-            case 1:addCurrentAccount();
-            break;
-            case 2:displayCurrentAccount();
-            break;
-            case 3:updateCurrentAccount();
-            break;
-            case 4:deleteCurrentAccount();
-            break;
-            case 0:
-                return;
+            System.out.println("\n1- Add Current Account" + "\n2 -Display Current Account" + " \n0- Get out " + "  Enter your choice :");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    addCurrentAccount();
+                    break;
+                case 2:
+                    displayCurrentAccount();
+                    break;
+                case 0:
+                    return;
                 default:
                     System.out.println("Invalid choice");
 
-        }}
+            }
+        }
 
     }
-}
+
+    public void addCurrentAccount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter account number :");
+        long accountNumber = scanner.nextLong();
+        System.out.print("Enter initial balance :");
+        double initialBalance = scanner.nextDouble();
+        System.out.print("Enter The ID of client :");
+        int cId = scanner.nextInt();
+        scanner.nextLine();
+        for (Client c : Client.client) {
+            if (cId == c.getId()) {
+                clients = c;
+                break;
+            }
+            if (clients == null) {
+                System.out.println("Client not found");
+                return;
+            }
+        }
+        CurrentAccount newAccount = new CurrentAccount(accountNumber, initialBalance, clients);
+        currentAccount.add(newAccount);
+
+        System.out.println("Current account has been created successfully for client: " + Client.client.getFirst().getFirstName() + " " + Client.client.getFirst().getLastName());
+
+
+    }
+
+    public static void displayCurrentAccount() {
+        System.out.println(" ------list of Current Accounts ------ -----");
+        for (CurrentAccount ca : currentAccount) {
+            System.out.println("CurrentAccount" +
+                    "\naccountNumber=" + getAccountNumber() +
+                    "\n balance=" + getInitialBalance() +
+                    "\n bankCharges=" + bankCharges +
+                    "\n client=" + Client.client.getFirst().getFirstName() +" " + Client.client.getLast().getLastName() + " (ID: " + Client.client.getFirst().getId() );
+        }
+    }
+
+
+                ;
+
+    }
+
